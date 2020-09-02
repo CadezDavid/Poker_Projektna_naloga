fc2707e32f5db7cce89911653a4848b9f3adc7ec
CadezDavid <david.cadez7@gmail.com>
2020-09-02 02:10:02 +0200

    Dodal funkcijo, s katero igra računalnik naključno

--- bot.py
+++ bot.py
@@ -2,9 +2,17 @@ import poker
 import random
 
 def izvedi_nakljucno_potezo(igra, igralec):
-    if random.choice([False, True, True]):
-        igra.stava[igralec] = max(list(igra.stava.values()))
-    elif random.choice([False, True]):
-        igra.igralci_v_igri[igralec] = False
+    if igra.odsek == 0:
+        if random.choice([False, True]):
+            igra.igralec_izenaci(igralec)
+        elif random.choice([False, False, False, False, True]):
+            igra.igralec_folda(igralec)
+        else:
+            igra.igralec_visa_za(igralec, random.choice([10, 20, 50]))
     else:
-        igra.stava[igralec] = max(list(igra.stava.values())) + 10
+        if random.choice([False, True]):
+            igra.igralec_izenaci(igralec)
+        elif random.choice([False, False, False, True]):
+            igra.igralec_folda(igralec)
+        else:
+            igra.igralec_visa_za(igralec, random.choice([10, 20, 50]))
